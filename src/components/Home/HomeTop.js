f4943f3948450e522057af085bbce64ff20988f9
kbeksss <kbeksss@outlook.com>
2020-03-08 18:39:55 +0600

    jlkkk

--- src/components/Home/HomeTop.js
+++ src/components/Home/HomeTop.js
@@ -1,11 +1,13 @@
 import React from 'react';
 import './HomeTop.css';
+import {NavLink} from "react-router-dom";
 
 const HomeTop = () => {
     return (
         <div className='HomeTop'>
             <div className='HomeTop_header'>
                 <h2>Welcome</h2>
+                <NavLink to'/admin'>kk</NavLink>
             </div>
         </div>
     );
